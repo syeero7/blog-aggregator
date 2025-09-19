a83a9d83169d8d4c527c92f7f06dfd28392c9270
emal <emalhasitha@proton.me>
2025-09-19 16:20:55 +0530

    feat: add config module

---
+++ internal/config/config.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = "gatorconfig.json"
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configFilepath, err := getConfigFilepath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	data, err := os.ReadFile(configFilepath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+func (cfg *Config) SetUser(name string) error {
+	oldConfig, err := Read()
+	if err != nil {
+		return err
+	}
+
+	oldConfig.CurrentUserName = name
+	newConfig, err := json.MarshalIndent(oldConfig, "", " ")
+	if err != nil {
+		return err
+	}
+
+	configFilepath, err := getConfigFilepath()
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(configFilepath, newConfig, 0o666); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getConfigFilepath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(homeDir, configFileName), nil
+}
--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/syeero7/blog-aggregator/internal/config"
+)
+
 func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%+v\n", cfg)
+
+	if err := cfg.SetUser("Emal"); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	cfg2, err := config.Read()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%+v\n", cfg2)
 }
